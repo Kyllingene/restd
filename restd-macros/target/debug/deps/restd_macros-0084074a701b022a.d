d9bf0d3e88e5e450bc38bbf8d91dbf4879cc4ca6
Kyllingene <fuzzymuffin343@gmail.com>
2024-09-05 10:47:51 -0700

    Oops

--- restd-macros/target/debug/deps/restd_macros-0084074a701b022a.d
+++
@@ -1,9 +0,0 @@
-/home/kyllingene/programming/rust/restd/restd-macros/target/debug/deps/librestd_macros-0084074a701b022a.rmeta: src/lib.rs Cargo.toml
-
-/home/kyllingene/programming/rust/restd/restd-macros/target/debug/deps/restd_macros-0084074a701b022a.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
