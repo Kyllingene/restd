a282d925e3167782fbd531040a2db939d75037d3
Kyllingene <fuzzymuffin343@gmail.com>
2024-09-21 08:45:02 -0700

    Fixed adapters

--- src/fmt/adapters.rs
+++ src/fmt/adapters.rs
@@ -8,14 +8,14 @@ pub struct StdDebug<T>(pub T);
 impl<T: fmt::Debug> Format<Debug> for StdDebug<T> {
     fn fmt(&self, f: &mut dyn Write, _: &Debug) -> Result {
         use fmt::Write as _;
-        core::write!(StdWrite(f), "{:?}", self.0).map_err(|_| Error)
+        core::write!(RestdWrite(f), "{:?}", self.0).map_err(|_| Error)
     }
 }
 
 impl<T: fmt::Debug> Format<Pretty> for StdDebug<T> {
     fn fmt(&self, f: &mut dyn Write, _: &Pretty) -> Result {
         use fmt::Write as _;
-        core::write!(StdWrite(f), "{:#?}", self.0).map_err(|_| Error)
+        core::write!(RestdWrite(f), "{:#?}", self.0).map_err(|_| Error)
     }
 }
 
@@ -27,7 +27,7 @@ super::derive!(struct StdDisplay<T!>(t));
 impl<T: fmt::Display> Format<Display> for StdDisplay<T> {
     fn fmt(&self, f: &mut dyn Write, _: &Display) -> Result {
         use fmt::Write as _;
-        core::write!(StdWrite(f), "{}", self.0).map_err(|_| Error)
+        core::write!(RestdWrite(f), "{}", self.0).map_err(|_| Error)
     }
 }
 
@@ -36,7 +36,22 @@ impl<T: fmt::Display> Format<Display> for StdDisplay<T> {
 pub struct StdWrite<T>(pub T);
 super::derive!(struct StdWrite<T!>(t));
 
-impl<T: Write> fmt::Write for StdWrite<T> {
+impl<T: fmt::Write> Write for StdWrite<T> {
+    fn write_str(&mut self, s: &str) -> Result {
+        fmt::Write::write_str(&mut self.0, s).map_err(|_| Error)
+    }
+
+    fn write_char(&mut self, ch: char) -> Result {
+        fmt::Write::write_char(&mut self.0, ch).map_err(|_| Error)
+    }
+}
+
+/// A wrapper around a type implementing [`restd::fmt::Write`] to make it
+/// implement [`core::fmt::Write`](Write).
+pub struct RestdWrite<T>(pub T);
+super::derive!(struct RestdWrite<T!>(t));
+
+impl<T: Write> fmt::Write for RestdWrite<T> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         Write::write_str(&mut self.0, s).map_err(|_| fmt::Error)
     }
